14fdc51b169d42eaa939050f0ded3029afd7a7b0
Alessio <avvertix@users.noreply.github.com>
2024-09-27 20:26:19 +0200

    Bump Node to 22.x (#84)
    * Bump Node to 22.x

--- scripts/node.sh
+++ scripts/node.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 # NODE JS
 mkdir -p /etc/apt/keyrings \
     && curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg \
-    && NODE_MAJOR=18 \
+    && NODE_MAJOR=22 \
     && echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list \
     && apt policy nodejs && apt update \
     && DEBIAN_FRONTEND=noninteractive apt-get install nodejs -yq \
