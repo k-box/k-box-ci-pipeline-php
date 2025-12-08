f5170897561ca6c6b4a8c37ce95610261932b5fd
Alessio <alessio.vertemati@gmail.com>
2025-12-08 09:45:47 +0100

    Update Node JS to v24

--- scripts/node.sh
+++ scripts/node.sh
@@ -2,13 +2,14 @@
 
 ## Original https://github.com/edbizarro/gitlab-ci-pipeline-php/blob/master/php/scripts/node.sh (c) Eduardo Bizarro
 ## Modification: updated to node 22
+## Modification: updated to node 24
 
 set -euo pipefail
 
 # NODE JS
 mkdir -p /etc/apt/keyrings \
     && curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg \
-    && NODE_MAJOR=22 \
+    && NODE_MAJOR=24 \
     && echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list \
     && apt policy nodejs && apt update \
     && DEBIAN_FRONTEND=noninteractive apt-get install nodejs -yq \
